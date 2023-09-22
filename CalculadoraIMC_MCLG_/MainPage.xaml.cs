2471fd8364f9cf0b0414f4b944c8a111d4c8e9ab
MariaLimon <limon185mari@gmail.com>
2023-09-21 20:01:53 -0700

    Agregar archivos de proyecto.

---
+++ CalculadoraIMC_MCLG_/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculadoraIMC_MCLG_
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        double imc;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            CalcularIMC(double.Parse(Peso.Text), double.Parse(Altura.Text));
+            //DECLARAR VARIABLES Y ADIGNAR VALOR
+            //var peso = float.Parse(Peso.Text);
+            //var altura = float.Parse(Altura.Text);
+            //REALIZAR CALCULO
+            //float imc = (peso / (altura * altura));
+
+            //ENVIAR RESULTADO
+            IMC.Text = imc.ToString();
+
+
+            //CONDICIONAR
+            string resultado = " ";
+            if (imc < 18.5)
+            {
+                resultado = "Bajo peso";
+            }
+            else if(imc>=18.5 && imc < 24.9 )
+            { 
+                resultado = "Peso normal";
+            
+            }
+            else if (imc >= 18.5 && imc <= 29.9)
+            {
+                resultado = "Sobre peso";
+
+            }
+            else if (imc > 29.9)
+            {
+                resultado = "Obesidad";
+
+            }
+
+            DisplayAlert("Resultado: ", resultado, "Quitar");
+        }
+
+        private void CalcularIMC(double p, double a)
+        {
+            imc = p / Math.Pow(a,2);
+        }
+    }
+}
